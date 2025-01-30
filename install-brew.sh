5da366478ad1ba84a5849f42e85c1470b10a6b20
Quang Nguyen <quang.sunlight@gmail.com>
2025-01-30 15:25:32 +0400

    feat(commitizen): add commitizen

--- install-brew.sh
+++ install-brew.sh
@@ -5,6 +5,10 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install nvm
+brew install commitizen
+nvm install 20
+npm install -g cz-conventional-changelog
+echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
 brew install stow
 stow .
 brew install --cask google-chrome
@@ -15,5 +19,3 @@ brew install --cask uhk-agent
 brew install --cask telegram
 brew install --cask rider
 brew install --cask karabiner-elements
-
-nvm install 20
