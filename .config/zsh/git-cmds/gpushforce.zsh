97209f3ae937f26eedd8c30c30567b4d128a759c
Quang Nguyen <quang.sunlight@gmail.com>
2025-09-25 07:15:13 +0400

    ✨ fix(git-cmds): update gpushforce to use --force-with-lease for safer pushes

--- .config/zsh/git-cmds/gpushforce.zsh
+++ .config/zsh/git-cmds/gpushforce.zsh
@@ -3,7 +3,7 @@
 gpushforce () {
     local branch=""
     eval "branch=\$(git rev-parse --abbrev-ref HEAD)"
-    local cmd="git push origin $branch --force"
+    local cmd="git push origin $branch --force-with-lease"
     echo -e "\e[32m$cmd\e[0m"  # Green color for selected item
     eval $cmd
 }
